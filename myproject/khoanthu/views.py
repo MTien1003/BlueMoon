fe68826c193d5b9a82a6b80f85347dc99eb5ec11
Thinh Dong <vanthinhgvo@gmail.com>
2025-12-18 13:46:42 +0700

    add view and update fee function

--- myproject/khoanthu/views.py
+++ myproject/khoanthu/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import KhoanThu
 
 # Create your views here.
@@ -25,3 +25,26 @@ def create_khoanthu(request):
         return redirect('khoanthu')
 
     return render(request, 'create_khoanthu.html')
+
+
+def xem_khoanthu(request, id):
+    """Xem chi tiết một khoản thu."""
+    khoanthu = get_object_or_404(KhoanThu, id=id)
+    return render(request, 'xem_khoanthu.html', {'khoanthu': khoanthu})
+
+
+def update_khoanthu(request, id):
+    """Cập nhật thông tin một khoản thu."""
+    khoanthu = get_object_or_404(KhoanThu, id=id)
+
+    if request.method == 'POST':
+        khoanthu.tenkhoanthu = request.POST['tenkhoanthu']
+        khoanthu.ngaytao = request.POST['ngaytao']
+        khoanthu.thoihan = request.POST['thoihan']
+        khoanthu.batbuoc = bool(request.POST.get('batbuoc'))
+        khoanthu.ghichu = request.POST.get('ghichu', '')
+        khoanthu.sotien = request.POST['sotien']
+        khoanthu.save()
+        return redirect('khoanthu')
+
+    return render(request, 'update_khoanthu.html', {'khoanthu': khoanthu})
