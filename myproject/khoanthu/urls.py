5b06dcc1acfd6046756dde01635eeb17d407ad77
Thinh Dong <vanthinhgvo@gmail.com>
2025-12-18 13:42:28 +0700

    add create fee page

--- myproject/khoanthu/urls.py
+++ myproject/khoanthu/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.khoanthu, name='khoanthu'),
+    path('create/', views.create_khoanthu, name='create_khoanthu'),
 ]
--- myproject/khoanthu/views.py
+++ myproject/khoanthu/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import KhoanThu
 
 # Create your views here.
 def khoanthu(request):
     khoanthus = KhoanThu.objects.order_by("-id")
     return render(request, 'khoanthu.html', {'khoanthus': khoanthus})
+
+
+def create_khoanthu(request):
+    """
+    Trang tạo mới KhoanThu.
+    Nhận dữ liệu từ form và lưu vào database, sau đó quay lại danh sách khoản thu.
+    """
+    if request.method == 'POST':
+        khoanthu = KhoanThu(
+            tenkhoanthu=request.POST['tenkhoanthu'],
+            ngaytao=request.POST['ngaytao'],
+            thoihan=request.POST['thoihan'],
+            batbuoc=bool(request.POST.get('batbuoc')),
+            ghichu=request.POST.get('ghichu', ''),
+            sotien=request.POST['sotien'],
+        )
+        khoanthu.save()
+        return redirect('khoanthu')
+
+    return render(request, 'create_khoanthu.html')
