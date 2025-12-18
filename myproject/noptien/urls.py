6a3a427b9735a0977891fc9744a8fbbf00d99a01
Thinh Dong <vanthinhgvo@gmail.com>
2025-12-18 14:11:14 +0700

    add function to create bill

--- myproject/noptien/urls.py
+++ myproject/noptien/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.hoadon, name='hoadon'),
+    path('create/', views.create_hoadon, name='create_hoadon'),
 ]
 
--- myproject/noptien/views.py
+++ myproject/noptien/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import NopTien
+from hokhau.models import HoKhau
+from khoanthu.models import KhoanThu
 
 
 def hoadon(request):
@@ -8,3 +10,31 @@ def hoadon(request):
     """
     hoadons = NopTien.objects.select_related('hokhau', 'khoanthu').order_by('-id')
     return render(request, 'hoadon.html', {'hoadons': hoadons})
+
+
+def create_hoadon(request):
+    """
+    Trang tạo mới hóa đơn (NopTien).
+    Chọn hộ khẩu, khoản thu từ dropdown, nhập người nộp, số tiền, ngày nộp.
+    """
+    hokhaus = HoKhau.objects.order_by('sohokhau')
+    khoanthus = KhoanThu.objects.order_by('tenkhoanthu')
+
+    if request.method == 'POST':
+        ho_khau = HoKhau.objects.get(id=request.POST['hokhau'])
+        khoan_thu = KhoanThu.objects.get(id=request.POST['khoanthu'])
+        hoadon = NopTien(
+            hokhau=ho_khau,
+            khoanthu=khoan_thu,
+            nguoinoptien=request.POST['nguoinoptien'],
+            sotien=request.POST['sotien'],
+            ngaynop=request.POST['ngaynop'],
+        )
+        hoadon.save()
+        return redirect('hoadon')
+
+    context = {
+        'hokhaus': hokhaus,
+        'khoanthus': khoanthus,
+    }
+    return render(request, 'create_hoadon.html', context)
