67e78c7e918215f764762eec101a5c3e5dece348
TSho <Trung.PT235853@sis.hust.edu.vn>
2025-12-27 16:29:30 +0700

    update home

--- myproject/home/views.py
+++ myproject/home/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from nhankhau.services import get_total_nhankhau
-
+from tamtrutamvang.views import dem_tam_tru_tam_vang
 
 def home(request):
     total_nhankhau = get_total_nhankhau()
+    so_luong_tam_tru, so_luong_tam_vang = dem_tam_tru_tam_vang()
     username = request.user.username
-    return render(request, 'index.html', {'total_nhankhau': total_nhankhau, 'username': username})
+    return render(request, 'index.html', {'total_nhankhau': total_nhankhau, 'username': username, 'so_luong_tam_tru': so_luong_tam_tru, 'so_luong_tam_vang': so_luong_tam_vang})
 
 def firstpage(request):
     return render(request, 'firstpage.html')
